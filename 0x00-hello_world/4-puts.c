6679781692cfa2c85b0b7da89dcad64957305de4
moha-mame <moha.mame04@gmail.com>
2022-09-08 02:33:20 -0700

    first commit on task 0 to 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  * Description: main - Prints out a programing ...........
+ *   * Return: 0 if success.
+ *    *
+**/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
