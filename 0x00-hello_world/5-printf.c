5a77e2a5f7065bf8db24aa292b9c53729416f519
moha-mame <moha.mame04@gmail.com>
2022-09-08 06:54:47 -0700

    try

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,24 +5,10 @@
  */
 int main(void)
 { 
-         printf("    short int is %2d bytes \n", sizeof(short int));
-         printf("          int is %2d bytes \n", sizeof(int));
-         printf("        int * is %2d bytes \n", sizeof(int *));
-	 printf("     long int is %2d bytes \n", sizeof(long int));
-	 printf("   long int * is %2d bytes \n", sizeof(long int *));
-	 printf("   signed int is %2d bytes \n", sizeof(signed int));
-	 printf(" unsigned int is %2d bytes \n", sizeof(unsigned int));
-	 printf("\n");
-	 printf("      float * is %2d bytes \n", sizeof(float *));
- 	 printf("      float * is %2d bytes \n", sizeof(float *));
-	 printf("       double is %2d bytes \n", sizeof(double));
-	 printf("     double * is %2d bytes \n", sizeof(double *));
-	 printf("  long double is %2d bytes \n", sizeof(long double));
-	 printf("\n");
-	 printf("  signed char is %2d bytes \n", sizeof(signed char));
-	 printf("         char is %2d bytes \n", sizeof(char));
-	 printf("       char * is %2d bytes \n", sizeof(char *));
-         printf("unsigned char is %2d bytes \n", sizeof(unsigned char));
-			
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
