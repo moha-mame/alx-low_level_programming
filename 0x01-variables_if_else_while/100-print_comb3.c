965edffdb0b8c3e79b08921b0b50677e9d211d41
moha-mame <moha.mame04@gmail.com>
2022-09-09 13:16:42 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - program that prints all possible different combinations of two digits
+ * Numbers must be separated by ,followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 48;
+	int j;
+	int com = 44;
+	int sp = 32;
+
+	for ( ; i <= 57; i++)
+	{
+		j = i + 1;
+	for ( ; j <= 57; j++)
+	{
+	putchar(i);
+	putchar(j);
+if (i != 56 || (i == 56 && j != 57))
+{
+	 putchar(com);
+	 putchar(sp);
+}
+}
+}
+putchar('\n');
+return (0);
+}
