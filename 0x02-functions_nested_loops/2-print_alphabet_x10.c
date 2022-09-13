7bce41731ba6be36b99a0d1f166795b8434ec50f
moha-mame <moha.mame04@gmail.com>
2022-09-13 04:18:09 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - func
+ *
+ *Return - 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+	int a = 97;
+
+	for (; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+		_putchar('\n');
+	}
+
+
+}
