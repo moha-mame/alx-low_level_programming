fbb9d77f5b0d386fa6186f65123e9fccaca8719f
moha-mame <moha.mame04@gmail.com>
2022-09-13 10:01:06 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints every minute of the day of Jack Bauer
+ *starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+
+	int i, j;
+
+	i = 0;
+	for (; i < 24 ; i++)
+	{
+	j = 0;
+	for (; j < 60 ; j++)
+	{
+		_putchar((i / 10) + i);
+		_putchar((i % 10) + i);
+		_putchar(':');
+		_putchar((j / 10) + i);
+		_putchar((j % 10) + i);
+		_putchar('\n');
+	}
+	}
+}
