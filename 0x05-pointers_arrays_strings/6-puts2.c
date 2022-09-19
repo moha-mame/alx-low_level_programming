e5ed8a9cbcd67675d785236adc7f71a1ededc213
moha-mame <moha.mame04@gmail.com>
2022-09-19 07:33:44 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *puts2 - prints every other character of a string.
+ *@str: the string to use.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	if (i % 2 == 0)
+	{
+	_putchar(str[i]);
+	}
+	}
+	_putchar('\n');
+}
