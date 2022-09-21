1046e6980f98318678c27161cd2f6df924380095
moha-mame <moha.mame04@gmail.com>
2022-09-21 09:33:15 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i,c=0;
+	int i, c = 0;
 
-	if(strlen(s1)==strlen(s2))
-    {
-    	for(i=0;s2[i];i++)  
-        {
-        	if(s1[i]==s2[i])
-        	 c++;
- 	}
- 	    if(c==i)
-          return 1;
-    }
-    return 0;
+	if (strlen(s1) == strlen(s2))
+{
+	for (i = 0; s2[i]; i++)
+	{
+	if (s1[i] == s2[i])
+		c++;
+	}
+	if (c == i)
+	return (1);
+}
+	return (0);
 }
 
