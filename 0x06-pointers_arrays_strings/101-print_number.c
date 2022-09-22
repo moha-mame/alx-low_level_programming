2a2bfe83aac821992119132cc8529dbac1b64d5d
moha-mame <moha.mame04@gmail.com>
2022-09-22 02:35:15 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+	_putchar('-');
+	n = -n;
+	}
+	if (n == 0)
+	_putchar('0');
+	if (n / 10)
+	print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
