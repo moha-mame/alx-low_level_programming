3e9351b362ec10e52d545fd6ff47a4faa2a28333
moha-mame <moha.mame04@gmail.com>
2022-09-22 06:46:00 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,7 @@
  */
 void print_number(int n)
 {
-	unsigned int i;	
+	unsigned int i;
 
 	i = n;
 	if (n < 0)
@@ -16,9 +16,9 @@ void print_number(int n)
 	_putchar('-');
 	i = -n;
 	}
-	if (i/10 !== 0)
+	if (i / 10 != 0)
 	{
 		print_number(i / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(i % 10 + '0');
 }
