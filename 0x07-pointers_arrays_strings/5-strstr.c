adf3f59b34d167e853fe82a2a3cce59da68a8a1b
moha-mame <moha.mame04@gmail.com>
2022-09-26 07:19:10 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* _strstr - function locate
+* @haystack: pointer to char
+* @needle: pointer to char
+* Return: 0
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	for (; haystack[i]; i++)
+	{
+		for (; needle[j] && (haystack[i] == needle[0]); j++)
+		{
+			if (haystack[i + j] == needle[j])
+			j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
