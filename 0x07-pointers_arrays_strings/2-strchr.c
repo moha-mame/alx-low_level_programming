5a8b60ea8a81b954964eb9423f14c5b0fe33f59c
moha-mame <moha.mame04@gmail.com>
2022-09-26 06:16:59 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,12 +7,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
-
-	for (; *(s + i) != '\0'; i++)
-	if (*(s + i) == c)
-	return (s + i);
-	if (*(s + i) == c)
-	return (s + i);
-	return (s);
+	for (; *s != '\0'; s++)
+	{
+	if (*s == c)
+		return (s);
+	}
+	if (*s == c)
+		return (s);
+	else
+		return (NULL);
 }
