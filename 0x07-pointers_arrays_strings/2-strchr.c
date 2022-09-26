0563983b023f3bdb9345dca143aed9db4b571b76
moha-mame <moha.mame04@gmail.com>
2022-09-26 06:30:53 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h> 
 /**
  * _strchr - locates a character in a string,
  * @s: string.
@@ -13,7 +14,9 @@ char *_strchr(char *s, char c)
 		return (s);
 	}
 	if (*s == c)
+	{
 		return (s);
+	}
 	else
-		return (NULL);
+	return (NULL);
 }
