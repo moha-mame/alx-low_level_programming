9c6ef66765b9a9b17c9b431439fe617002ca2f16
moha-mame <moha.mame04@gmail.com>
2022-09-30 05:40:28 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+*main - print number of argument
+*@argc: - argument
+*@argv: - argumrnt ponter
+*Return: - 0
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
