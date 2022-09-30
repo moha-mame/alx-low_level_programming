896c8c92daade94b8b87833df8256089d4c0f231
moha-mame <moha.mame04@gmail.com>
2022-09-30 05:07:36 -0700

    tsk 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main -  writes program name
+* @argc: arguement
+* @argv: array of pointers
+* Return: 0
+**/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
