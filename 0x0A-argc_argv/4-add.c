9fc196ffaf66684e587669b3f28dc4001ee06967
moha-mame <moha.mame04@gmail.com>
2022-09-30 10:54:59 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 /**
- * main - adds positive numbers
- * @argc: argument ct
- * @argv: argument for values
- *
- * Return: Always 0 (Success)
- */
-int main(int argc, char *argv[])
+ * check_digit - checks if a given char is number or not
+ * @a: input char
+ * Return: int
+ **/
+
+int check_digit(char *a)
 {
-	int i, sum = 0;
+	int i, num, len;
 
-	if (argc < 1)
-		return (0);
-
-	for (i = 1; i < argc; i++)
+	i = 0;
+	num = 0;
+	len = strlen(a);
+	for (; i < len; i++)
 	{
-		if (!atoi(argv[i]))
+		if (a[i] < '0' || a[i] > '9')
 		{
-			printf("%s\n", "Error");
-			return (1);
+			return (-1);
 		}
-		sum += atoi(argv[i]);
+		else
+			num = num * 10 + (a[i] - '0');
 	}
-	printf("%d\n", sum);
-
-	return (0);
+	return (num);
 }
 
+/**
+ * main -  program that adds positive numbers
+ * @argc: arguement count
+ * @argv: argument vector
+ * Return: int
+ **/
+
+int main(int argc, char *argv[])
+{
+	int i, num, res;
+
+	res = 0;
+	for (i = 1; i < argc; i++)
+	{
+		num = check_digit(argv[i]);
+		if (num == -1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		res += num;
+	}
+	printf("%d\n", res);
+	return (0);
+}
