bfe947b417b649edfb60ac60c5ba00aed26e64b4
moha-mame <moha.mame04@gmail.com>
2022-10-04 01:37:47 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,12 +1,19 @@
 #include"main.h"
 #include <stdlib.h>
 #include <string.h>
-char * _strdup( char* src){
-    char * dest = malloc( (1+strlen(src))*sizeof(char));
-    char * toReturn = dest;
+/**
+*_strdup - the string given as a parameter.
+*@src: - string
+*Return: - string
+*/
+char *_strdup(char *src)
+{
+	char *dest = malloc((1 + strlen(src)) * sizeof(char));
+	char *toReturn = dest;
 
-    while (*src){
-        *dest++ = *src++;
-    } 
-  return toReturn;
-} 
+	while (*src)
+	{
+	*dest++ = *src++;
+	}
+	return (toReturn);
+}
