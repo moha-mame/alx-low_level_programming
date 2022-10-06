2cd6dfa68e4601c7aa9562416a1747573a92cfa3
moha-mame <moha.mame04@gmail.com>
2022-10-06 02:26:48 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,41 @@
 #include <stdlib.h>
 #include "main.h"
-char *string_nconcat (char *s1, char *s2, unsigned int n)
+
+/**
+* string_nconcat - concatenates two strings
+* @s1: string one
+* @s2: string two
+* @n: bytes
+* Return: poniter
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
-  unsigned int i, j, x, y, z;
-  char *p;
+	unsigned int i, j, x, y, z;
+	char *p;
 
-  if (s1 == NULL)
-    s1 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		;
 
-  if (s2 == NULL)
-    s2 = "";
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-  for (i = 0; s1[i] != '\0'; i++)
-    ;
+	if (n > j)
+		n = j;
+	x = i + n;
+	p = malloc((x + 1));
+	if (p == NULL)
+		return (NULL);
+	for (x = 0; x < i; x++)
+		p[x] = s1[x];
+	for (y = i, z = 0; y < i + n; y++, z++)
+		p[y] = s2[z];
 
-  for (j = 0; s2[j] != '\0'; j++)
-    ;
-  if (n > j)
-    n = j;
+	p[i + j] = '\0';
 
-  x = i + n;
-
-  p = malloc ((x + 1));
-
-  if (p == NULL)
-    return (NULL);
-
-  for (x = 0; x < i; x++)
-    p[x] = s1[x];
-
-  for (y = i, z = 0; y < i + n; y++, z++)
-    p[y] = s2[z];
-
-  p[i + j] = '\0';
-
-  return (p);
+	return (p);
 }
