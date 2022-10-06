9e36fb991e9a6b32450c3d9e1100aebdd0975ce8
moha-mame <moha.mame04@gmail.com>
2022-10-06 12:50:10 -0700

    task4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,10 +14,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *p;
 
 	if (new_size > old_size)
-
+	{
 	p = malloc(new_size);
 		free(ptr);
 		return (p);
+	}
 	if (new_size == old_size)
 	{
 		return (ptr);
