3c3cfabb8eba9938c0144753def4c44ad206fe5c
moha-mame <moha.mame04@gmail.com>
2022-10-10 08:03:02 -0700

    task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,14 +6,14 @@
  * @d: dog structure
  */
 
-void print_dog (struct dog *d)
+void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	printf("Name: (nil)\n");
-    else
-    {
+	if (d != NULL)
+	{
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
-  }
+	}
 }
