1f7d6ac6580133f9861eaccc8e5b1a112874967e
moha-mame <moha.mame04@gmail.com>
2022-10-13 08:48:58 -0700

    task2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Print numbers using a separator
+ * @separator: The string separator
+ * @n: The numbers
+ * @...: The list
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	int i;
+	char *p;
+
+	va_start(strings, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL && i != 0)
+			printf("%s", separator);
+		p = va_arg(strings, char *);
+			printf("%s", (p == NULL) ? "(nil)" : p);
+	}
+	printf("\n");
+	va_end(strings);
+}
