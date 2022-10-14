09df60158e56a049c8512eb53a397dab38437e45
moha-mame <moha.mame04@gmail.com>
2022-10-14 01:05:53 -0700

    variadic_functions.h update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,26 @@
-#ifndef VARFUNC_H
-#define VARFUNC_H
+#ifndef VARFUNC
+#define VARFUNC
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct p - checker struct
+ * @p: possibility
+ * @f: Function to be used
+ */
+
+typedef struct p
+{
+  char *p;
+  void (*f)(va_list);
+} checker;
+
+void print_c(va_list);
+void print_i(va_list);
+void print_f(va_list);
+void print_s(va_list);
+
 #endif
