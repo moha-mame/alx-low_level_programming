cfc4e2c87bf39b505e6b802e15d244de22e765dd
moha-mame <moha.mame04@gmail.com>
2022-10-20 09:45:00 -0700

    task 5 done fukenn betty fi

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,8 +2,8 @@
 /**
  * before_main - prints from init by the loader before main
  */
- void before_main() __attribute__((constructor));
- void before_main()
+void before_main(void) __attribute__((constructor));
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
