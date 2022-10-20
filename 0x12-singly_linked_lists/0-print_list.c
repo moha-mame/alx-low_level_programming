a20aa0de2bde642c4a51abe0afec59c2e8847d5d
moha-mame <moha.mame04@gmail.com>
2022-10-20 03:25:38 -0700

    0-print_list 2nd try

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,10 +9,18 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	for (i = 0; h; i++)
+	for (i = 0; h != NULL; i++)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+	if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+		}
 	}
 	return (i);
 }
