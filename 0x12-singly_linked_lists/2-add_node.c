2fe363b4dadcde6bb0a5b02be73b14ee3e967bd2
moha-mame <moha.mame04@gmail.com>
2022-10-20 05:17:36 -0700

    2-add_node 4th try

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,13 +8,14 @@
  *
  * Return: the address of the new element, or NULL if it fails
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	unsigned int len = 0;
 
-for (; str[len]; len++)
-            ;
+	for (; str[len]; len++)
+		;
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
