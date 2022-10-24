e239c1c986e116358bf717a9223afaf9870aedda
moha-mame <moha.mame04@gmail.com>
2022-10-24 11:05:56 -0700

    0-print_listint fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,19 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t len;
 
-	if (h == NULL)
-		return (0);
-
-	for (len = 0; h != NULL; h = h->next, len++)
-		printf("%d\n", h->n);
-
-	return (len);
+	if (h)
+	{
+		if (h->str)
+			printf("[%d] %s%s", h->len, h->str, "\n");
+		else
+			printf("[%d] %s%s", 0, "(nil)", "\n");
+		if (h->next)
+		{
+			return (1 + print_list(h->next));
+		}
+		return (1);
+	}
+	return (0);
+}
 }
